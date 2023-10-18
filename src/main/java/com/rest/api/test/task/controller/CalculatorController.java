77d227f06423f2a8a978fae63d068976f9d824dc
Dmitriy <dima.2000600@gmail.com>
2023-10-18 19:05:06 +0300

    Update controller method with changed return type and error handling

--- src/main/java/com/rest/api/test/task/controller/CalculatorController.java
+++ src/main/java/com/rest/api/test/task/controller/CalculatorController.java
@@ -5,12 +5,15 @@ import com.rest.api.test.task.exception.CityNotFoundException;
 import com.rest.api.test.task.model.DistanceRequest;
 import com.rest.api.test.task.service.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CalculatorController {
@@ -22,17 +25,28 @@ public class CalculatorController {
     }
 
     @PostMapping("/calculate")
-    public ResponseEntity<?> calculate(@RequestBody DistanceRequest distanceRequest) {
-
+    public ResponseEntity<Map<String, Object>>calculate(@RequestBody DistanceRequest distanceRequest) {
+        Map<String, Object> result = new HashMap<>();
         try {
             if (distanceRequest.getCalculationType().equals("Crowflight")) {
-                return ResponseEntity.ok(calculateService.calculateCrowFlightDistances(distanceRequest));
+                result.put("Crowflight", calculateService.calculateCrowFlightDistances(distanceRequest));
+                return ResponseEntity.ok(result);
             } else if (distanceRequest.getCalculationType().equals("Distance matrix")) {
-                return ResponseEntity.ok(Arrays.asList(calculateService.calculateMatrixDistances(distanceRequest)));
+                result.put("Distance matrix", calculateService.calculateMatrixDistances(distanceRequest));
+                return ResponseEntity.ok(result);
+            } else if (distanceRequest.getCalculationType().equals("All")) {
+                result.put("Crowflight", calculateService.calculateCrowFlightDistances(distanceRequest));
+                result.put("Distance matrix", calculateService.calculateMatrixDistances(distanceRequest));
+                return ResponseEntity.ok(result);
+            }else {
+                result.put("Error", "Invalid calculation type");
+                return ResponseEntity.badRequest().body(result);
             }
         } catch (CityNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, City not found, e);
         }
-        return ResponseEntity.badRequest().body("Bad request");
     }
+
 }
+
