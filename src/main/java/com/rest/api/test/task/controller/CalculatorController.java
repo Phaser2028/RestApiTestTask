afad387846254eb41724a115b8a7bd1e41b67cf2
Dmitriy <dima.2000600@gmail.com>
2023-10-18 20:09:22 +0300

    Add entity mapping

--- src/main/java/com/rest/api/test/task/controller/CalculatorController.java
+++ src/main/java/com/rest/api/test/task/controller/CalculatorController.java
@@ -1,7 +1,9 @@
 package com.rest.api.test.task.controller;
 
 
+import com.rest.api.test.task.dto.DistanceDTO;
 import com.rest.api.test.task.exception.CityNotFoundException;
+import com.rest.api.test.task.mapper.Mapper;
 import com.rest.api.test.task.model.DistanceRequest;
 import com.rest.api.test.task.service.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,39 +15,70 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class CalculatorController {
     private final CalculateService calculateService;
+    private final Mapper mapper;
 
     @Autowired
-    public CalculatorController(CalculateService calculateService) {
+    public CalculatorController(CalculateService calculateService, Mapper mapper) {
         this.calculateService = calculateService;
+        this.mapper = mapper;
     }
 
     @PostMapping("/calculate")
     public ResponseEntity<Map<String, Object>>calculate(@RequestBody DistanceRequest distanceRequest) {
+
         Map<String, Object> result = new HashMap<>();
+        List<DistanceDTO> resultsDistanceMatrix;
+        List<DistanceDTO> resultsCrowFlight;
+
+
         try {
             if (distanceRequest.getCalculationType().equals("Crowflight")) {
-                result.put("Crowflight", calculateService.calculateCrowFlightDistances(distanceRequest));
+
+                resultsCrowFlight = calculateService.calculateCrowFlightDistances(distanceRequest).stream()
+                        .map(mapper::distanceToDistanceDTO).collect(Collectors.toList());
+                result.put("Crowflight", resultsCrowFlight);
+
                 return ResponseEntity.ok(result);
+
             } else if (distanceRequest.getCalculationType().equals("Distance matrix")) {
-                result.put("Distance matrix", calculateService.calculateMatrixDistances(distanceRequest));
+
+                resultsDistanceMatrix = calculateService.calculateMatrixDistances(distanceRequest).stream()
+                        .map(mapper::distanceToDistanceDTO).collect(Collectors.toList());
+                result.put("Distance matrix", resultsDistanceMatrix);
+
                 return ResponseEntity.ok(result);
+
             } else if (distanceRequest.getCalculationType().equals("All")) {
-                result.put("Crowflight", calculateService.calculateCrowFlightDistances(distanceRequest));
-                result.put("Distance matrix", calculateService.calculateMatrixDistances(distanceRequest));
+
+                resultsDistanceMatrix = calculateService.calculateMatrixDistances(distanceRequest).stream()
+                        .map(mapper::distanceToDistanceDTO).collect(Collectors.toList());
+                resultsCrowFlight = calculateService.calculateCrowFlightDistances(distanceRequest).stream()
+                        .map(mapper::distanceToDistanceDTO).collect(Collectors.toList());
+
+                result.put("Crowflight", resultsCrowFlight);
+                result.put("Distance matrix", resultsDistanceMatrix);
+
+
                 return ResponseEntity.ok(result);
+
+
             }else {
                 result.put("Error", "Invalid calculation type");
                 return ResponseEntity.badRequest().body(result);
             }
+
         } catch (CityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
             //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, City not found, e);
         }
+
     }
 
 }
